d0dbf62af0034e1a687eafddbff7bdcd18461565
tuan.lba@c-plus.cloud <tuan.lba@c-plus.cloud>
2020-08-10 16:36:35 +0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-martin"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://hbm8ebqm75.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_ojThGvGK4",
+        APP_CLIENT_ID: "4st5ocos3il1glnph585ecla69",
+        IDENTITY_POOL_ID: "us-east-1:b9846fa9-5224-4116-b94a-85039535e499"
+    }
+};

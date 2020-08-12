27af5daac38ef51c8f24c2db0c09be68f3b91b02
tuan.lba@c-plus.cloud <tuan.lba@c-plus.cloud>
2020-08-12 09:46:21 +0700

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,7 @@ import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 
@@ -58,6 +59,14 @@ export default function Home() {
             <div className="lander">
                 <h1>Scratch</h1>
                 <p>A simple note taking app</p>
+                <div>
+                    <Link to="/login" className="btn btn-info btn-lg">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="btn btn-success btn-lg">
+                        Signup
+                    </Link>
+                </div>
             </div>
         );
     }
